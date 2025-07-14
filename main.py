e5c0d2aba7b653e42728da23e193f8d724b8e275
Tanaya Tipre <tanayatipre8@gmail.com>
2025-07-14 20:05:19 +0530

    model trainer added

--- main.py
+++ main.py
@@ -1,10 +1,43 @@
 from MLProject import logger
 from MLProject.pipeline.data_ingestion_01 import DataIngestionTrainingPipeline
+from MLProject.pipeline.data_validation_02 import DataValidationTrainingPipeline
+from MLProject.pipeline.data_transformation_03 import DataTransformationTrainingPipeline
+from MLProject.pipeline.model_trainer_04 import ModelTrainerTrainingPipeline
 
 STAGE_NAME = "Data Ingestion Stage"
 try:
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
     obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Data Validation Stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataValidationTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Data Transformation Stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataTransformationTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Model Trainer Stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = ModelTrainerTrainingPipeline()
     obj.main()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
--- src/MLProject/config/configuration.py
+++ src/MLProject/config/configuration.py
@@ -1,6 +1,11 @@
 from MLProject.constants import *
 from MLProject.utils.common import read_yaml, create_directories
 from MLProject.entity.config_entity import DataIngestionConfig
+from MLProject.entity.config_entity import DataValidationConfig
+from MLProject.entity.config_entity import DataTransformationConfig
+from MLProject.entity.config_entity import ModelTrainerConfig
+
+
 class ConfigurationManager:
     def __init__(
         self,
@@ -27,3 +32,52 @@ class ConfigurationManager:
         )
 
         return data_ingestion_config
+    
+    def get_data_validation_config(self) -> DataValidationConfig:
+        config = self.config.data_validation
+        schema = self.schema.COLUMNS
+
+        create_directories([config.root_dir])
+
+        data_validation_config = DataValidationConfig(
+            root_dir=config.root_dir,
+            STATUS_FILE=config.STATUS_FILE,
+            unzip_data_dir=config.unzip_data_dir,
+            all_schema=schema
+        )
+
+        return data_validation_config
+    
+    def get_data_transformation_config(self) -> DataTransformationConfig:
+        config = self.config.data_transformation
+
+        create_directories([config.root_dir])
+
+        data_transformation_config = DataTransformationConfig(
+            root_dir=config.root_dir,
+            data_path=config.data_path
+        )
+
+        return data_transformation_config
+    
+
+    def get_model_trainer_config(self) -> ModelTrainerConfig:
+        config = self.config.model_trainer
+        params = self.params.ElasticNet
+        schema = self.schema.TARGET_COLUMN
+
+        create_directories([config.root_dir])
+
+        model_trainer_config = ModelTrainerConfig(
+            root_dir = config.root_dir,
+            train_data_path = config.train_data_path,
+            test_data_path = config.test_data_path,
+            model_name = config.model_name,
+            alpha = params.alpha,
+            l1_ratio = params.l1_ratio,
+            target_column = schema.name
+        )
+
+        return model_trainer_config
+    
+    
